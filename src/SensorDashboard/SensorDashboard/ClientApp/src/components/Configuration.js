dbb85ed5523e0a4ceade66cee3488214514c5ca5
Gary Evans <garymervynevans@hotmail.com>
2019-10-16 21:54:08 +0100

    change Configuration component to functional component

--- src/SensorDashboard/SensorDashboard/ClientApp/src/components/Configuration.js
+++ src/SensorDashboard/SensorDashboard/ClientApp/src/components/Configuration.js
@@ -1,43 +1,34 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
 
-export class Configuration extends Component {
-    static displayName = Configuration.name;
-
-    static renderSensorsTable(sensorConfig) {
-        return (
-            <table className='table table-striped'>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Manufacturer</th>
-                        <th>Host Device</th>
-                        <th>Units</th>
+export function Configuration(props) {
+    let contents = props.loadingConfig
+        ? <p><em>Loading...</em></p>
+        : <table className='table table-striped'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Manufacturer</th>
+                    <th>Host Device</th>
+                    <th>Units</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props. sensorConfig.map(sensor =>
+                    <tr key={sensor.name}>
+                        <td>{sensor.name}</td>
+                        <td>{sensor.manufacturer}</td>
+                        <td>{sensor.hostDevice}</td>
+                        <td>{sensor.sensorUnits}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    {sensorConfig.map(sensor =>
-                        <tr key={sensor.name}>
-                            <td>{sensor.name}</td>
-                            <td>{sensor.manufacturer}</td>
-                            <td>{sensor.hostDevice}</td>
-                            <td>{sensor.sensorUnits}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
+                )}
+            </tbody>
+        </table>;
 
-    render() {
-        let contents = this.props.loadingConfig
-            ? <p><em>Loading...</em></p>
-            : Configuration.renderSensorsTable(this.props.sensorConfig);
-
-        return (
-            <div>
-                <h1>Sensor Configuration</h1>
-                {contents}
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Sensor Configuration</h1>
+            {contents}
+        </div>
+    );
 }
+
