af0824d8a4de2e1a71482e3dcce2b11638df8015
Gary Evans <garymervynevans@hotmail.com>
2019-11-19 19:12:32 +0000

    make the dashboard layout less hardcoded, more dynamic

--- src/SensorDashboard/SensorDashboard/ClientApp/src/components/Dashboard.js
+++ src/SensorDashboard/SensorDashboard/ClientApp/src/components/Dashboard.js
@@ -14,26 +14,60 @@ import useSubscribeSensorStream from './useSubscribeSensorStream.js';
 import './Dashboard.css';
 
 const SensorValueWithConnection = withDashboardItemCard(withConnectionIndicator(SensorValue));
-const SensorValueWithConnectionMinMax = withSensorMinMax(withDashboardItemCard(withConnectionIndicator(SensorValue)));
 const SensorChartWithConnection = withDashboardItemCard(withConnectionIndicator(SensorChart));
 
+const SensorTypes = Object.freeze({
+    VALUE: 'value',
+    CHART: 'chart'
+});
+
+// TODO: these will be stored server-side, and will be dynamically updatable
+const sensorConfigItems =
+    [
+        { sensorName: "temperature_room_1", chartType: SensorTypes.VALUE, minMax: false },
+        { sensorName: "temperature_room_2", chartType: SensorTypes.VALUE, minMax: true },
+        { sensorName: "humidity_room_2", chartType: SensorTypes.VALUE, minMax: false },
+        { sensorName: "temperature_room_1", chartType: SensorTypes.CHART, minMax: false },
+        { sensorName: "temperature_room_2", chartType: SensorTypes.CHART, minMax: false },
+        { sensorName: "humidity_room_2", chartType: SensorTypes.CHART, minMax: true }
+    ];
 
 export function Dashboard(props) {
     const hubConnection = useHubConnect('/streamHub');
 
-    // TODO: dictionary of sensor names
-    let temperatureRoom1Msg = useSubscribeSensorStream(hubConnection, 'StartListening', 'temperature_room_1');
-    let temperatureRoom2Msg = useSubscribeSensorStream(hubConnection, 'StartListening', 'temperature_room_2');
-    let humidityRoom2Msg = useSubscribeSensorStream(hubConnection, 'StartListening', 'humidity_room_2');
+    const sensorNames = [...new Set(sensorConfigItems.map(x => x.sensorName))];
+
+    let sensorDatas = {};
+
+    for (const sensorName of sensorNames)
+    {
+        // TODO: breaks rule of hooks even if order maintained? How to cope with dashboard config (layout) changes?
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        let sensorData = useSubscribeSensorStream(hubConnection, 'StartListening', sensorName);
+        sensorDatas[sensorName] = sensorData;
+    }
 
     return (
         <div className='grid-container'>
-            <SensorValueWithConnection className='grid-item' {...props} latestMsg={temperatureRoom1Msg.latestMsg} sensorName='temperature_room_1' hubConnection={hubConnection} />
-            <SensorValueWithConnectionMinMax className='grid-item' {...props} latestMsg={temperatureRoom2Msg.latestMsg} sensorName='temperature_room_2' hubConnection={hubConnection} />
-            <SensorValueWithConnection className='grid-item' {...props} latestMsg={humidityRoom2Msg.latestMsg} sensorName='humidity_room_2' hubConnection={hubConnection} />
-            <SensorChartWithConnection className='grid-item' {...props} latestMsg={temperatureRoom1Msg.latestMsg} sensorName='temperature_room_1' hubConnection={hubConnection} />
-            <SensorChartWithConnection className='grid-item' {...props} latestMsg={temperatureRoom2Msg.latestMsg} sensorName='temperature_room_2' hubConnection={hubConnection} />
-            <SensorChartWithConnection className='grid-item' {...props} latestMsg={humidityRoom2Msg.latestMsg} sensorName='humidity_room_2' hubConnection={hubConnection} />
+            {
+                sensorConfigItems.map((sci, index) => {
+                    let component = sci.chartType === SensorTypes.VALUE
+                        ? SensorValueWithConnection
+                        : SensorChartWithConnection;
+
+                    // NOTE: can't do this, as it seems a new instance of the chart is created, and the useState hook
+                    //  doesn't realise it's really the same instance - the seriesData is always empty on render.
+                    //let component = sci.chartType === SensorTypes.VALUE
+                    //    ? withDashboardItemCard(withConnectionIndicator(SensorValue))
+                    //    : withDashboardItemCard(withConnectionIndicator(SensorChart));
+
+                    if (sci.minMax) {
+                        component = withSensorMinMax(component);
+                    }
+
+                    return component({ ...props, className: 'grid-item', key: index, latestMsg: sensorDatas[sci.sensorName].latestMsg, sensorName: sci.sensorName, hubConnection: hubConnection });
+                })
+            }
         </div>
     );
 }
